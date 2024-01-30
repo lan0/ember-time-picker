bb70418cca65f20f584371e07def496db3e08914
Michael Kerr <michael.kerr@mainmatter.com>
2024-01-30 18:10:53 +0100

    Update ember try

--- config/ember-try.js
+++ config/ember-try.js
@@ -6,18 +6,34 @@ module.exports = async function () {
   return {
     scenarios: [
       {
-        name: 'ember-lts-3.16',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.16.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-3.20',
         npm: {
           devDependencies: {
             'ember-source': '~3.20.5',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-3.24',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.24.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.28.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.0',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.0.0',
           },
         },
       },
